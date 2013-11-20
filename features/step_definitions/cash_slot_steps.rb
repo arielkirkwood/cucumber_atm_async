a3cab527edcebef8cc454d80d24ccc83a7ef8229
Ariel Kirkwood <ariel.kirkwood@me.com>
2013-11-20 13:55:53 -0500

    additional step definitions for prevent_overdraw feature

--- features/step_definitions/cash_slot_steps.rb
+++ features/step_definitions/cash_slot_steps.rb
@@ -1,3 +1,7 @@
 Then(/^(#{CAPTURE_CASH_AMOUNT}) should be dispensed$/) do |amount|
   cash_slot.contents.should == amount
 end
+
+Then(/^nothing should be dispensed$/) do
+  pending # express the regexp above with the code you wish you had
+end
--- features/step_definitions/teller_steps.rb
+++ features/step_definitions/teller_steps.rb
@@ -1,3 +1,7 @@
 When(/^I withdraw (#{CAPTURE_CASH_AMOUNT})$/) do |amount|
   teller.withdraw_from(my_account, amount)
 end
+
+Then(/^I should be told that I have insufficient funds in my account$/) do
+  pending # express the regexp above with the code you wish you had
+end
