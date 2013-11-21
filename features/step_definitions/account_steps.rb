a06826d670aa84ea35873d20d10b86befa715b85
Ariel Kirkwood <ariel.kirkwood@me.com>
2013-11-21 11:44:17 -0500

    removed 'clarifying' text from a balance-related acct step

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -3,6 +3,5 @@ Given(/^my account has been credited with (#{CAPTURE_CASH_AMOUNT})$/) do |amount
 end
 
 Then(/^the balance of my account should be (#{CAPTURE_CASH_AMOUNT})$/) do |amount|
-  my_account.balance.should eq(amount),
-    "Expected the balance to be #{amount} but it was #{my_account.balance}"
+  my_account.balance.should eq(amount)
 end
