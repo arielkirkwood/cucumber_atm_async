5cbcb0fc0926d2d1c1761b0ac0e7e3c0a8334090
Ariel Kirkwood <ariel.kirkwood@me.com>
2013-11-20 22:00:03 -0500

    updated UI with system message area; withdraw_from has updated logic

--- lib/cucumber_atm.rb
+++ lib/cucumber_atm.rb
@@ -13,13 +13,21 @@ class Account
 end
 
 class Teller
+  attr_accessor :message
+
   def initialize(cash_slot)
     @cash_slot = cash_slot
   end
 
-  def withdraw_from(account, amount)
-    account.debit(amount)
-    @cash_slot.dispense(amount)
+  def withdraw_from(account, amount_requested)
+    if account.balance >= amount_requested
+      account.debit(amount_requested)
+      @cash_slot.dispense(amount_requested)
+      @message = "Success!"
+    else
+      @cash_slot.dispense(0)
+      @message = "Sorry! You have insufficient funds."
+    end
   end
 end
 
@@ -36,42 +44,7 @@ end
 require 'sinatra'
 
 get '/' do
-  %{
-  <html>
-    <body>
-      <form action="/withdraw" method="post">
-        <label for="amount">Amount</label>
-        <input type="hidden" id="amount" name="amount" value="20">
-        <button type="submit">$20</button>
-      </form>
-      <form action="/withdraw" method="post">
-        <label for="amount">Amount</label>
-        <input type="hidden" id="amount" name="amount" value="40">
-        <button type="submit">$40</button>
-      </form>
-      <form action="/withdraw" method="post">
-        <label for="amount">Amount</label>
-        <input type="hidden" id="amount" name="amount" value="60">
-        <button type="submit">$60</button>
-      </form>
-      <form action="/withdraw" method="post">
-        <label for="amount">Amount</label>
-        <input type="hidden" id="amount" name="amount" value="80">
-        <button type="submit">$80</button>
-      </form>
-      <form action="/withdraw" method="post">
-        <label for="amount">Amount</label>
-        <input type="hidden" id="amount" name="amount" value="100">
-        <button type="submit">$100</button>
-      </form>
-      <form action="/withdraw" method="post">
-        <label for="amount">Amount</label>
-        <input type="hidden" id="amount" name="amount" value="200">
-        <button type="submit">$200</button>
-      </form>
-    </body>
-  </html>
-  }
+  erb :index
 end
 
 set :cash_slot, CashSlot.new
@@ -82,4 +55,6 @@ end
 post '/withdraw' do
   teller = Teller.new(settings.cash_slot)
   teller.withdraw_from(settings.account, params[:amount].to_i)
+  @message = teller.message
+  erb :index
 end
