3be2d7c47eb94cbcc6e40d7ba6a6d0c741be0a99
Ariel Kirkwood <ariel.kirkwood@me.com>
2013-11-21 19:14:24 -0500

    changed Account into an ActiveRecord class

--- lib/cucumber_atm.rb
+++ lib/cucumber_atm.rb
@@ -1,25 +1,6 @@
 require_relative 'transaction_queue'
 require_relative 'balance_store'
 
-class Account
-  def initialize
-    @queue = TransactionQueue.new
-    @balance_store = BalanceStore.new
-  end
-
-  def balance
-    @balance_store.balance
-  end
-
-  def credit(amount)
-    @queue.write("+#{amount}")
-  end
-
-  def debit(amount)
-    @queue.write("-#{amount}")
-  end
-end
-
 class Teller
   attr_accessor :message
 
